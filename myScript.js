f74caf4c8c8e7234f9c4907933183f72b7c2730b
Monet-Aaliyah Harkins <monetharkins02@gmail.com>
2019-07-17 10:35:25 -0500

    first commit

---
+++ myScript.js
@@ -0,0 +1,47 @@
+// // alert("Hello World")
+// const name = "Monet";
+// const number = 2;
+// var walls;
+// walls = "Great Wall"
+// function timesThree(num) {
+// return num * 3;
+// };
+// function timesFive(num) {return num * 5};
+// function helloWorld() { alert("Hello World")} ;
+//
+// if ( 5 == 5) {
+//   alert("Yay!");
+// };
+//
+// var number = -25;
+// var msg = "Error!";
+// var letter = 'm';
+
+// let text = ""
+// for (i = 0; i < 5; i++) {
+//     text += "Num: " + i;
+// }
+//
+// let num = 0
+//
+// for (var i = 0; i < 5; i++){
+//   num += i
+// // }
+//
+// let text =
+// while (var i = 0; i < 10, i++) {
+// //     text += "Num: " + i;
+// //     i += 2;
+// // }
+// let num = 0;
+// console.log("Hello World");
+// debugger;
+// num = 1;
+// console.log(num)
+
+function showTime() {
+  document.getElementById('demo').innerHTML = Date();
+};
+ function cool() {
+   document.getELementByID('demo').
+ }
